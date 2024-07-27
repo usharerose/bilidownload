6b691bf9381980c947a04aaa4cabb4050b2d3fde
usharerose <ushareroses@gmail.com>
2024-07-27 18:32:19 +0800

    feat: get details with epid URL

--- bilidownload/video_service.py
+++ bilidownload/video_service.py
@@ -5,22 +5,31 @@ from enum import Enum
 import re
 from typing import Optional
 
-from .proxy import GetVideoInfoResponse, ProxyService
+from .proxy import (
+    GetBangumiDetailResponse,
+    GetVideoInfoResponse,
+    ProxyService
+)
 
 
 class VideoType(Enum):
 
     VIDEO = 'video'
+    BANGUMI = 'bangumi'
 
 
 BVID_LENGTH = 9
 VIDEO_URL_BV_PATTERN = re.compile(fr'/video/(BV1[a-zA-Z0-9]{{{BVID_LENGTH}}})')
 VIDEO_URL_AV_PATTERN = re.compile(r'/video/av(\d+)')
+VIDEO_URL_EP_PATTERN_STRING = r'/play/ep(\d+)'
+VIDEO_URL_EP_PATTERN = re.compile(VIDEO_URL_EP_PATTERN_STRING)
+VIDEO_URL_BANGUMI_EP_PATTERN = re.compile(r'/bangumi' + VIDEO_URL_EP_PATTERN_STRING)
 
 
 VIDEO_TYPE_MAPPING = {
     VIDEO_URL_BV_PATTERN: VideoType.VIDEO,
-    VIDEO_URL_AV_PATTERN: VideoType.VIDEO
+    VIDEO_URL_AV_PATTERN: VideoType.VIDEO,
+    VIDEO_URL_BANGUMI_EP_PATTERN: VideoType.BANGUMI
 }
 
 
@@ -52,6 +61,13 @@ class VideoService:
         return int(search_result.group(1))
 
     @classmethod
+    def get_epid(cls, url: str) -> Optional[int]:
+        search_result = VIDEO_URL_EP_PATTERN.search(url)
+        if search_result is None:
+            return None
+        return int(search_result.group(1))
+
+    @classmethod
     def get_video_info(cls, url: str, session_data: Optional[str] = None) -> GetVideoInfoResponse:
         video_type = cls.get_video_type(url)
         if video_type is None:
@@ -74,3 +90,15 @@ class VideoService:
             params.update({'aid': aid})
         video_info_response_dm = ProxyService.get_video_info_data(session_data=session_data, **params)
         return video_info_response_dm
+
+    @classmethod
+    def _get_bangumi_video_info(
+        cls,
+        url: str,
+        session_data: Optional[str] = None
+    ) -> GetBangumiDetailResponse:
+        params = {}
+        epid = cls.get_epid(url)
+        params.update({'epid': epid})
+        bangumi_response_dm = ProxyService.get_bangumi_info_data(session_data=session_data, **params)
+        return bangumi_response_dm
